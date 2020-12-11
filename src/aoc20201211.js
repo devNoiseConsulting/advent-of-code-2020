0ec4ece129ce4618e81af0f4f7a351c9066294d0
Michael Flynn <flynnmj@devnoise.com>
2020-12-11 09:16:34 -0500

    Day 11

---
+++ src/aoc20201211.js
@@ -0,0 +1,132 @@
+var fs = require("fs");
+var path = require("path");
+
+let file = path.join(__dirname, "..", "data", "day11.txt");
+var layout = fs
+  .readFileSync(file, "utf-8")
+  .trim()
+  .split("\n")
+  .map((row) => row.split(""));
+
+let offsets = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
+
+newLayout = layout.map((row) => row.slice());
+let changing = true;
+while (changing) {
+  changing = false;
+  newLayout = layout.map((row) => row.slice());
+  newLayout = newLayout.map((row, x) =>
+    row.map((column, y) => {
+      if (column == ".") {
+        return column;
+      }
+
+      let occupiedNeighbors = offsets.reduce((acc, [dx, dy]) => {
+        if (layout[x + dx]) {
+          if (layout[x + dx][y + dy] == "#") {
+            acc++;
+          }
+        }
+        return acc;
+      }, 0);
+
+      let seat = column;
+      if (column == "#" && occupiedNeighbors >= 4) {
+        seat = "L";
+      }
+      if (column == "L" && occupiedNeighbors == 0) {
+        seat = "#";
+      }
+      if (seat !== column) {
+        changing = true;
+      }
+      return seat;
+    })
+  );
+  layout = newLayout;
+}
+
+let seatCount = newLayout.reduce((acc, row) => {
+  return (acc += row.reduce((acc, column) => {
+    if (column == "#") {
+      acc++;
+    }
+    return acc;
+  }, 0));
+}, 0);
+
+console.log(seatCount);
+
+layout = fs
+  .readFileSync(file, "utf-8")
+  .trim()
+  .split("\n")
+  .map((row) => row.split(""));
+
+changing = true;
+while (changing) {
+  changing = false;
+  newLayout = layout.map((row) => row.slice());
+  newLayout = newLayout.map((row, x) =>
+    row.map((column, y) => {
+      if (column == ".") {
+        return column;
+      }
+
+      let occupiedNeighbors = offsets
+        .map(([dx, dy]) => {
+          let line = ".";
+          let lineX = x + dx;
+          let lineY = y + dy;
+          while (line == "." && layout[lineX] && layout[lineX][lineY]) {
+            if (layout[lineX][lineY] == "L" || layout[lineX][lineY] == "#") {
+              line = layout[lineX][lineY];
+            }
+            lineX += dx;
+            lineY += dy;
+          }
+
+          return line;
+        })
+        .reduce((acc, line) => {
+          if (line == "#") {
+            acc++;
+          }
+          return acc;
+        }, 0);
+
+      let seat = column;
+      if (column == "#" && occupiedNeighbors >= 5) {
+        seat = "L";
+      }
+      if (column == "L" && occupiedNeighbors == 0) {
+        seat = "#";
+      }
+      if (seat !== column) {
+        changing = true;
+      }
+      return seat;
+    })
+  );
+  layout = newLayout;
+}
+
+seatCount = newLayout.reduce((acc, row) => {
+  return (acc += row.reduce((acc, column) => {
+    if (column == "#") {
+      acc++;
+    }
+    return acc;
+  }, 0));
+}, 0);
+
+console.log(seatCount);
